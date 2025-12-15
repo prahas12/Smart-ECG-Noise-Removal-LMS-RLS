66fe2a84649b8a07f3242bc387d5f44e10975763
Prahas Sharma <prahas2006@gmail.com>
2025-12-15 20:49:39 +0530

    Update lms_filter.m

--- lms_filter.m
+++ lms_filter.m
@@ -1,15 +1,19 @@
-
 function [y,e] = lms_filter(d,x)
-mu = 0.01;
-M  = 8;
+
+mu = 0.01;        % Step size
+M  = 8;           % Filter order
 N  = length(d);
+
 w = zeros(M,1);
 y = zeros(1,N);
 e = zeros(1,N);
+
 for n = M:N
     x_vec = x(n:-1:n-M+1).';
     y(n)  = w.' * x_vec;
     e(n)  = d(n) - y(n);
     w     = w + mu * e(n) * x_vec;
 end
+
 end
+
